4522169c4793d8d8db738e8c4b2d66780abc6531
Alex Talbott <atalbott@usc.edu>
2021-03-30 11:57:53 -0400

    added users and auth middleware

--- config/db.js
+++ config/db.js
@@ -4,11 +4,12 @@ const db = config.get('mongoURI');
 
 const connectDb = async () => {
     try {
-      await mongoose.connect(db, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-          useCreateIndex: true
-      });
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true
+		});
 
       console.log('MongoDb Connected....');
     } catch (error) {
