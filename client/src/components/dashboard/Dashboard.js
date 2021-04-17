4e7a57de34ea474a6ec6dbf2591a69e09d394083
Alex Talbott <atalbott@usc.edu>
2021-04-17 16:42:38 -0400

    added profiles

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,17 +2,21 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layouts/Spinner';
-import { getCurrentProfile } from '../../actions/profile';
+import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
+import { deleteAccount, getCurrentProfile } from '../../actions/profile';
 import { Link } from 'react-router-dom';
 
 const Dashboard = ({
 	getCurrentProfile,
+	deleteAccount,
 	auth: { user },
 	profile: { profile, loading },
 }) => {
 	useEffect(() => {
 		getCurrentProfile();
-	}, []);
+	}, [getCurrentProfile]);
 	return loading && profile == null ? (
 		<Spinner />
 	) : (
@@ -22,7 +26,17 @@ const Dashboard = ({
 				<i className="fas fa-user">Welcome {user && user.name}</i>
 			</p>
 			{profile !== null ? (
-				<Fragment>has</Fragment>
+				<Fragment>
+					<DashboardActions />
+					<Experience experience={profile.experience} />
+					<Education education={profile.education} />
+
+					<div className="my-2">
+						<button className="btn btn-danger" onClick={() => deleteAccount()}>
+							<i className="fas fa-user-minus"></i> Delete My Account
+						</button>
+					</div>
+				</Fragment>
 			) : (
 				<Fragment>
 					<p>You have not set up a profile, please add some info</p>
@@ -39,6 +53,7 @@ Dashboard.propTypes = {
 	getCurrentProfile: PropTypes.func.isRequired,
 	auth: PropTypes.object.isRequired,
 	profile: PropTypes.object.isRequired,
+	deleteAccount: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
@@ -46,4 +61,6 @@ const mapStateToProps = (state) => ({
 	profile: state.profile,
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
+	Dashboard
+);
--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -8,13 +8,16 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 	const authLinks = (
 		<ul>
 			<li>
+				<Link to="/profiles">Developers</Link>
+			</li>
+			<li>
 				<Link to="/dashboard">
 					<i className="fas fa-user"></i>{' '}
 					<span className="hide-sm">Dashboard</span>{' '}
 				</Link>
 			</li>
 			<li>
-				<a onClick={logout} to="#!">
+				<a onClick={logout} href="#!">
 					<i className="fas fa-sign-out-alt"></i>
 					<span className="hide-sm"> Logout</span>
 				</a>
@@ -25,7 +28,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 	const guestLinks = (
 		<ul>
 			<li>
-				<Link to="#!">Developers</Link>
+				<Link to="/profiles">Developers</Link>
 			</li>
 			<li>
 				<Link to="/register">Register</Link>
